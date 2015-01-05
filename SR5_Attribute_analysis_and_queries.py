cd62a84aaf55fd61cfd9ce14cfb5d34c20c88ff4
Justin Tyme Dejesus <justin.tyme.dejesus@gmail.com>
2015-01-05 01:50:13 -0600

    Got Scatter plot working.

--- SR5_Attribute_analysis_and_queries.py
+++ SR5_Attribute_analysis_and_queries.py
@@ -21,7 +21,7 @@ df = pd.read_csv('Shadowrun_Attributes.csv')
 def Show_BoxPlot(DataFrame):
     ''' call this to show boxplots of my passed dataframe'''
     groupby = ['Physical Limit', 'Mental Limit', 'Social Limit', 'Attribute Sum']
-
+ 
     for i in range(len(groupby)):
 
         DataFrame.boxplot(column=['Body', 'Agility','Reaction', 'Strength',
@@ -43,14 +43,17 @@ def Show_Histogram(DataFrame):
     # we don't need sums for this one
     DataFrame.pop('Attribute Sum')
 
-    # stacked type
-    plt.figure()
+    # stacked type - this is colored*
+    # The stack is somewhat hard to read..
+    #plt.figure()
     DataFrame.plot(kind='hist',stacked=True,
                    bins=20)
-    # indpendant type
+    
+    # indpendant Histograms
+        
     DataFrame.hist(sharex=True,
                    sharey=True,
-                   bins=50,)
+                   bins=20)
     
     plt.show()
 
@@ -62,14 +65,23 @@ def Show_Histogram(DataFrame):
 
 # Scatter Plot
 def Show_ScatterPlot(DataFrame):
-    '''Show an area plot when called'''
+    '''Show a series of scatter plots when called'''
 
     # we don't need sums for this one
     DataFrame.pop('Attribute Sum')
     
-    plt.figure()
-    DataFrame.plot(kind='area', stacked=False)
+    # create a 2-dimensional list
+    attribute = ['Body', 'Agility','Reaction', 'Strength','Willpower', 'Logic','Intuition', 'Charisma']
+    limit = ['Physical Limit', 'Mental Limit', 'Social Limit']
+
+    # all 8 attributes on x plotted against all 3 limits on y, each in their own graph
+    # takes a bit to load 24 scatter plots that do all these comparisons though.
+    for i in range(len(attribute)):
+        for j in range(len(limit)):
+            DataFrame.plot(kind='scatter',x=attribute[i], y=limit[j])
 
     plt.show()
 
-Show_AreaPlot(df)
+    # These will show form of the spread of min/max attributes needed for any such attribute..this was cool!
+
+Show_ScatterPlot(df)
